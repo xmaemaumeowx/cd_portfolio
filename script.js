01fba767c8bedcde2d2208fbfb6579bcb9076fd0
maemaumeow <105728752+xmaemaumeowx@users.noreply.github.com>
2025-01-17 06:10:17 +0800

    version 2
    fixed problem in toast, added tooltip

---
+++ script.js
@@ -0,0 +1,21 @@
+function submitForm() {
+    // Get form data
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+  
+    // Display success message 
+    alert("Form submitted successfully!");
+  
+    // Reload the page after alert is closed
+    window.location.reload(); 
+  
+    return false; // Prevent default form submission
+  }
+//tool tip
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
